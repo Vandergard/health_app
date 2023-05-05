e929292462f526a9f87a3a1873c48bd1d6161271
Vadim Terentyev <vterentyev@humans.net>
2023-05-05 22:59:11 +0600

    Make drop down list for search exercises

--- app/javascript/packs/exercise_search.js
+++ app/javascript/packs/exercise_search.js
@@ -3,6 +3,10 @@ import $ from "jquery";
 $(document).ready(function () {
     $("#exercise-search").on("input", function () {
         let query = $(this).val();
+        if (query.length === 0) {
+            clearSearchResults();
+            return;
+        }
         if (query.length >= 3) {
             $.ajax({
                 url: "/exercises/search",
@@ -26,11 +30,16 @@ $(document).ready(function () {
             return;
         }
 
-        const resultList = $("<ul></ul>");
-        results.forEach((result) => {
-            resultList.append(`<li>${result.name}</li>`); // Замените `result.name` на актуальное свойство объекта, если имя упражнения хранится в другом поле
-        });
+        const resultList = $("<div class='dropdown-menu show'></div>");
+    results.forEach((result) => {
+        resultList.append(`<div class='dropdown-item'>${result.name}</div>`); // Замените `result.name` на актуальное свойство объекта, если имя упражнения хранится в другом поле
+    });
 
         resultsContainer.append(resultList);
+    }
+
+    function clearSearchResults() {
+        const resultsContainer = $("#search-results");
+        resultsContainer.empty();
     }
 });
