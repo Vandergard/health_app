100cf8279832bb050ef9664b9b265bc8353437d0
Vadim Terentyev <vterentyev@humans.net>
2023-05-21 21:51:52 +0600

    Fix training saving process

--- app/controllers/trainings_controller.rb
+++ app/controllers/trainings_controller.rb
@@ -6,7 +6,7 @@ class TrainingsController < ApplicationController
   end
 
   def create
-    training_params = params.require(:training).permit(:name, :category, :difficulty, :description, training_sets_attributes: [:exercise_id, :reps, :duration, :set_rest])
+    training_params = params.require(:training).permit(:name, :category, :difficulty, :description, training_sets_attributes: [:exercise_id, :reps, :duration, :set_rest, :count, :description ])
   
     training = Training.new(
       name: training_params[:name],
@@ -16,14 +16,27 @@ class TrainingsController < ApplicationController
     )
   
     if training.save
+      # this if need to convert json array to ruby hash (1:{}, 2:{})
+      if training_params[:training_sets_attributes].is_a?(Array)
+        training_sets_attributes_hash = Hash[training_params[:training_sets_attributes].map.with_index do |set_data, index|
+          [index.to_s, set_data]
+        end]
+        training_params[:training_sets_attributes] = training_sets_attributes_hash
+      end
       training_params[:training_sets_attributes].each do |index, set_data|
-        set = TrainingSet.create(
+        if set_data
+          set = TrainingSet.create(
           exercise_id: set_data[:exercise_id].to_i,
           reps: set_data[:reps],
           duration: set_data[:duration],
-          set_rest: set_data[:set_rest]
-        )
-        training.sets << set
+          set_rest: set_data[:set_rest],
+          count: set_data[:count],
+          description: set_data[:description]
+          )
+          training.sets << set
+        else
+          Rails.logger.debug "Unexpected nil in training_sets_attributes at index #{index}"
+        end
       end 
       if training.save
         render json: { status: "success", message: "Training created successfully." }, status: :created
@@ -36,6 +49,7 @@ class TrainingsController < ApplicationController
   end  
 
   def show
+    @training = Training.includes(training_sets: :exercise).find(params[:id])
   end
 
   def edit
